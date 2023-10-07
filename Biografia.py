aa2d276034a3e7031e14d123a74c810f26527786
LivMaster13 <livier.orozco@uabc.edu.mx>
2023-10-07 15:59:12 -0700

    Ejercicio 4 Biografia informativa

---
+++ Biografia.py
@@ -0,0 +1,12 @@
+# Solicitar al usuario que ingrese información personal
+nombre = input("Ingresa tu nombre: ")
+fecha_nacimiento = input("Ingresa tu fecha de nacimiento: ")
+lugar_nacimiento = input("Ingresa tu lugar de nacimiento: ")
+ocupacion = input("Ingresa tu ocupación: ")
+
+# Mostrar la información biográfica que ingreso el usuario
+print("\nBiografía:")
+print(f"Nombre: {nombre}")
+print(f"Fecha de Nacimiento: {fecha_nacimiento}")
+print(f"Lugar de Nacimiento: {lugar_nacimiento}")
+print(f"Ocupación: {ocupacion}")
