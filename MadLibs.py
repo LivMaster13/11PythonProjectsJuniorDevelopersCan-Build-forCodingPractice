052c7e23bec8b37d21a602fd3821e51d4c61e45b
LivMaster13 <livier.orozco@uabc.edu.mx>
2023-10-07 16:01:48 -0700

    Ejercicio 2 Juego Mad Libs

---
+++ MadLibs.py
@@ -0,0 +1,12 @@
+# Pedir al usuario que ingrese palabras
+nombre = input("Ingresa un nombre: ")
+verbo = input("Ingresa un verbo: ")
+sustantivo = input("Ingresa un sustantivo: ")
+adjetivo = input("Ingresa un adjetivo: ")
+
+# Crear una historia utilizando las palabras proporcionadas por el usuario
+historia = f"{nombre} {verbo} {sustantivo} {adjetivo}."
+
+print("\nTu historia ha sido generada espero que sea de tu agrado:):")
+print(historia)
+
