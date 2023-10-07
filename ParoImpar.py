e7f722ca1be8c60be97d136ab4a1d56e82a452cd
LivMaster13 <livier.orozco@uabc.edu.mx>
2023-10-07 15:56:17 -0700

    Ejercicio 1 Par o Impar

---
+++ ParoImpar.py
@@ -0,0 +1,11 @@
+
+# Solicitar al usuario ingresar un número
+
+numero = int(input("Ingrese un número: "))
+
+# Comprueba si el número es par o impar
+if numero % 2 == 0:
+    print(f" {numero} tiene como resultado un número par.")
+else:
+    print(f" { numero} tiene como resultado un número impar.")
+
